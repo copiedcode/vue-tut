3ee9249e30e66d841896cad37381e5c693616756
copiedcode <raphael.webdesign@outlook.com>
2020-05-03 22:15:53 +0200

    add columns for later canban

--- main.js
+++ main.js
@@ -1,3 +1,47 @@
+Vue.component('modal', {
+    template:`
+        <div class="modal is-active">
+        <div class="modal-background"></div>
+        <div class="modal-content">
+            <div class="box">
+                <slot></slot>
+            </div>
+        </div>
+        <button class="modal-close is-large" @click="$emit('close')" aria-label="close"></button>
+    </div>
+    
+    `
+});
+
+
+
+
+
+Vue.component('message', {
+    props: ['title', 'body'],
+    data() {
+        return {
+            isVisible: true
+        };
+    },
+    template: `
+        <article class="message" v-show="isVisible">
+            <div class="message-header">
+                {{ title }}
+                <button class="delete" @click="isVisible = false" aria-label="delete"></button>
+            </div>
+            <div class="message-body">
+            {{ body }}
+            </div>
+        </article>
+    `
+});
+
+
+
+
+
+
 Vue.component('task-list', {
     template: '<div><task v-for="task in tasks">{{ task.task }}</task></div>',
 
@@ -27,6 +71,7 @@ let app = new Vue({
     el: '#root',
 
     data:  {
+        showModal: false,
         tasks: [
             { description: 'Aufstehen', completed: true},
             { description: 'Essen kaufen', completed: false},
